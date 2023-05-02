085c9a654c2106a9725684cbaedb578d51188c1a
Oleksandr Patuk <oleksandr.patuk@gmail.com>
2023-05-02 15:43:21 +0300

    Improve predefined configs

--- inventory_generator.py
+++ inventory_generator.py
@@ -1,11 +1,13 @@
 import sys
 import os
+from typing import List
 
 configs = {
     "masters": "1",
-    "workers": "",
+    "workers": "1:10",
     "etcd": "1",
     "nfs": "1",
+    "calico": "",
     "total": 1,
     "file": "hosts.ini",
     "start_ip": "172.18.8.10"
@@ -18,6 +20,23 @@ def parse_args(arguments):
             raise ValueError(f"Invalid argument format: {arg}")
         key, value = arg.split("=")
         configs[key] = value
+
+
+def generate_ips(ip: str, steps: int) -> List[str]:
+    # Split the IP address into octets and convert each octet to an integer
+    octets = [int(octet) for octet in ip.split(".")]
+    # Convert the octets to a single integer
+    start_ip = (octets[0] << 24) + (octets[1] << 16) + (octets[2] << 8) + octets[3]
+    # Generate a list of `steps` IP addresses
+    ips = []
+    for i in range(start_ip, start_ip + steps):
+        # Convert the integer IP address back to a string
+        octet1 = (i >> 24) & 0xff
+        octet2 = (i >> 16) & 0xff
+        octet3 = (i >> 8) & 0xff
+        octet4 = i & 0xff
+        ips.append(f"{octet1}.{octet2}.{octet3}.{octet4}")
+    return ips
 
 
 def generate_configfile(**kwargs):
